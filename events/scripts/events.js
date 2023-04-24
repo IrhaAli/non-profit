eff87853134fbb48a69cb68ddfc31c189b311c55
open-meadow <rohan.varughese@protonmail.com>
2023-04-24 12:53:35 -0400

    Create boilerplate server

--- events/scripts/events.js
+++ events/scripts/events.js
@@ -1,4 +1,11 @@
+// import eventList from "../db/events.json"
+
 $(() => {
   console.log("jQuery loaded");
   // console.log("eventlist: ", eventList);
+
+  fetch('../db/events.json')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
 });
