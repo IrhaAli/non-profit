8b17f910544197e1c3f1e709a1eca6c59ac6039a
open-meadow <rohan.varughese@protonmail.com>
2023-04-24 13:26:38 -0400

    create public folder

---
+++ public/events/scripts/events.js
@@ -0,0 +1,11 @@
+// import eventList from "../db/events.json"
+
+$(() => {
+  console.log("jQuery loaded");
+  // console.log("eventlist: ", eventList);
+
+  // fetch('http://localhost:8080/events')
+  // .then(response => response.json())
+  // .then(data => console.log(data))
+  // .catch(error => console.error(error));
+});
--- server.js
+++ server.js
@@ -1,7 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With",
+    "Content-Type",
+    "Accept"
+  );
+  next();
+});
+app.use(express.static("public"));
+
+app.get("/events", (req, res) => {
+  const events = require("./public/events/db/events.json");
+  console.log("These are events: ", events);
+  res.json(events);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening at http://localhost:${PORT}`);
-})
+});
