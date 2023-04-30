85741cda56a2d141d44a201f08511b00810a6887
Irha Ali <irhaali_97@hotmail.ca>
2023-04-30 17:08:28 -0400

    Events delete button

--- scripts/events.js
+++ scripts/events.js
@@ -1,32 +1,34 @@
-$(() => {
-  // To display all events
-  fetch("../db/events.json")
-    .then(response => response.json())
-    .then((events) => {
-      loadEvents(events)
-    })
-
+$(async () => {
   // Display add event form option
   let loggedIn = true;
   const $showForm = $('form');
   $showForm.css("display", (loggedIn) ? "block" : "none");
 
+  // To display all events
+  await fetch("../db/events.json")
+    .then(response => response.json())
+    .then((events) => {
+      loadEvents(events, loggedIn)
+    })
 
   // When event is added
   $("form").on("submit", submitEvent);
+
+  // When event is removed
+  $(".del-eve").on("click", deleteEvent);
 });
 
-const loadEvents = (eventsObj) => {
-  for (let singleEvent in eventsObj) {
-    const event = eventsObj[singleEvent];
-    const $event = createEventElement(event);
+const loadEvents = (eventsArr, loggedIn) => {
+  for (let id = 0; id < eventsArr.length; id++) {
+    const event = eventsArr[id];
+    const $event = createEventElement(event, id, loggedIn);
     $(".events").append($event);
   }
 };
 
-const createEventElement = function(event) {
+const createEventElement = function(event, id, loggedIn) {
   const $event = $(`
-  <div class="events--card">
+  <div class="events--card" id="${id}">
   <img src="${event.image_url}" class="events--card--img" />
   <div class="events--card--body">
     <h2 class="events--card--body--title">${event.title}</h2>
@@ -36,6 +38,7 @@ const createEventElement = function(event) {
     </div>
     <p class="events--card--body--content">${event.description}</p>
     </div>
+    ${(loggedIn) ? '<button class="del-eve"><i class="fa fa-trash-o" style="font-size:48px;color:red"></i></button>' : ''}
   </div>`);
   return $event;
 };
@@ -43,7 +46,6 @@ const createEventElement = function(event) {
 const submitEvent = function(event) {
   // Initial settings
   event.preventDefault();
-
 
   // Get the event
   const formData = $(".eventForm").serializeArray();
@@ -60,3 +62,15 @@ const submitEvent = function(event) {
   const $event = createEventElement(eventDetails);
   $(".events").append($event);
 };
+
+const deleteEvent = function(event) {
+  // Initial settings
+  event.preventDefault();
+  const eventId = $(this).closest('.events--card').attr('id');
+
+  // Remove event from JSON file
+  // removeEvent(eventId);
+
+  // Update event list (frontend)
+   $(`#${eventId}`).css("display", 'none');
+}
